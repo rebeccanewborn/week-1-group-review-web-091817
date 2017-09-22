e0281a71de5edeea9b5789fe444a05bf3927e4ee
rebeccanewborn <rjn58@cornell.edu>
2017-09-22 09:38:50 -0400

    done discussion 0922

--- review-question-2.rb
+++ review-question-2.rb
@@ -1,28 +1,44 @@
 # Finish the implementation of the Car class so it has the functionality described below
 
 class Car
+  attr_accessor :make, :model
+  @@all = []
 
+  def initialize(make:, model:)
+    @make = make
+    @model = model
+    @@all << self
+  end
 
+  def drive
+    "VROOOOOOOOOOOOM!"
+  end
+
+  def self.all
+    @@all
+  end
 
 end
 
-car = Car.new("volvo", "lightening")
-car.make
-#=> "volvo"
-car.model
-#=> "ligthening"
+# car1 = Car.new("volvo", "lightening")
+# car2 = Car.new("bmw", "x5")
+# car3 = Car.new("jeep", "grand cherokee")
+# car1.make
+# #=> "volvo"
+# car1.model
+# #=> "ligthening"
 
-car.drive
-# => "VROOOOOOOOOOOOM!"
+# car2.drive
+# # => "VROOOOOOOOOOOOM!"
 
-Car.all
-#=> [car1, car2, car3]
+# Car.all
+# #=> [car1, car2, car3]
 
-BONUS:
 
-Car.new(make: "volvo", model: "lightening")
 
-car.make
-#=> "volvo"
-car.model
-#=> "ligthening"
+car4 = Car.new(model: "italia", make: "ferrari")
+
+car4.make
+#=> "ferrari"
+car4.model
+#=> "italia"
--- review-question-3.rb
+++ review-question-3.rb
@@ -2,6 +2,45 @@
 # you should have a User class, a Photo class and a comment class
 
 
+class Photo
+  attr_accessor :user, :comments
+
+  def initialize
+    @comments = []
+
+  end
+
+  def user=(username)
+    @user = username
+    username.photos << self
+  end
+
+  def make_comment(string)
+    Comment.new(string)
+    comments << string
+  end
+
+end
+
+class Comment
+  @@all = []
+  def initialize(string)
+    @string = string
+    @@all << string
+  end
+  def self.all
+    @@all
+  end
+end
+
+class User
+  attr_accessor :name, :photos
+  def initialize(name)
+    @name = name
+    @photos = []
+  end
+end
+
 photo = Photo.new
 user = User.new("Sophie")
 photo.user = user
@@ -17,6 +56,10 @@ photo.comments
 photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
 photo.comments
 # => [<comment1>]
+
+
+photo.make_comment("cool!")
+
 
 Comment.all
 #=> [<comment1>]
